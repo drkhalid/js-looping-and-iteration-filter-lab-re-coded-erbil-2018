50132f12f6896383848015768aac7505841195fa
khalid jamal <kh.jamal95@gmail.com>
2018-08-12 18:55:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function findMatching (list, name) {
 
 function fuzzyMatch(list,name){
   return list.filter(function(driverName){
-    return driverName[0,name.length] === name
+    return driverName.slice[0,name.length] === name
   })
   
 }

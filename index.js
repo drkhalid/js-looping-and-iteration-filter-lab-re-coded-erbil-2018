2f682a436f4920e64f051c7985d5b87c0fe508ee
khalid jamal <kh.jamal95@gmail.com>
2018-08-12 19:00:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file
+function findMatching (list, name) {
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(list,name){
+  return list.filter(function(driverName){
+    return driverName.slice(0,name.length) === name
+  })
+  
+}
+
+function matchName(list, prop){
+  return list.filter(function (driverProp){
+    return driverProp.name.toLowerCase() === prop.toLowerCase()
+  });
+}
+
+
